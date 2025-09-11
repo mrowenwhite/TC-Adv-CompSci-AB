b3d1fab1e251c3b1c4a90809fa5d61aa24523f54
O-white1 <white.o3@stu.janesville.k12.wi.us>
2025-09-11 17:03:31 -0500

    404a done

--- src/Langdocs/Prog404a.java
+++ src/Langdocs/Prog404a.java
@@ -14,35 +14,54 @@ public class Prog404a {
 
         List<Integer> pos = nums.stream().filter(n -> n>=0).toList();
         List<Integer> neg = nums.stream().filter(n -> n<0).toList();
+        List<Integer> small = (pos.size()>=neg.size()) ? pos : neg;
+        List<Integer> big =   (small.equals(pos))      ? neg : pos;
 
-        for (int lcv = 0; lcv < (Math.max(pos.size(), neg.size())); lcv++) {
-            System.out.println(pos.get(lcv) + " " + neg.get(lcv)); // data files are the same # of ints
+        pos = new ArrayList<>(pos);
+        neg = new ArrayList<>(neg);
+
+
+        System.out.println("Positive:\tNegative:");
+
+
+        int lcv = 0;
+        while (lcv < big.size()) {
+            while (lcv < small.size()-1) {
+                System.out.println(pos.get(lcv) + "\t\t\t\t" + neg.get(lcv));
+                lcv++;
+            }
+            System.out.println(big.equals(pos) ? big.get(lcv) : "\t\t\t\t" + big.get(lcv));
+            lcv++;
         }
+
+
     }
 }
+
 /*
-3 -8
-66 -16
-54 -56
-22 -34
-19 -22
-21 -55
-34 -3
-64 -55
-55 -76
-9 -45
-39 -3
-54 -66
-33 -54
-8 -22
-10 -19
-56 -21
-34 -34
-22 -64
-55 -55
-3 -9
-55 -89
-76 -54
-45 -33
+Positive:	Negative:
+3				-8
+66				-16
+54				-56
+22				-34
+19				-22
+21				-55
+34				-3
+64				-55
+55				-76
+9				-45
+39				-3
+54				-66
+33				-54
+8				-22
+10				-19
+56				-21
+34				-34
+22				-64
+55				-55
+3				-9
+55				-89
+76				-54
+				-33
 
  */
