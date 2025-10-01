6255c07ad81ede3d4615a613df090ff0ee70f9a0
white.o3 <white.o3@stu.janesville.k12.wi.us>
2025-10-01 10:47:12 -0500

    492h and 470t underway + linked lists deletion

--- src/Q1/Langdocs/Prog470t.java
+++ src/Q1/Langdocs/Prog470t.java
@@ -1,5 +1,4 @@
 package Q1.Langdocs;
-/*
 import java.util.*;
 import java.io.*;
 
@@ -7,8 +6,8 @@ public class Prog470t {
     public static void main(String[] args) throws IOException {
         Scanner file1 = new Scanner(new File("Langdats/prg470t1.dat"));
         Scanner file2 = new Scanner(new File("Langdats/prg470t2.dat"));
-        int[][] mat   = new int[4][4];
-        int[][] mat2  = new int[4][4];
+        int[][] mat = new int[4][4];
+        int[][] mat2 = new int[4][4];
 
         while (file1.hasNextLine()) {
             for (int i = 0; i < 4; i++)
@@ -21,23 +20,68 @@ public class Prog470t {
                     mat2[i][j] = file2.nextInt();
         }
 
+        for (int[] row : mat) {
+            for (int i : row)
+                System.out.print(i + "\t");
+            System.out.println("\t\t" + getSum(row));
+        }
+        if (isMagicSquare(mat)) System.out.println("Matrix 1 is a Magic Square!");
+        else System.out.println("Matrix 1 is not a Magic Square..");
+        System.out.println("\n\n\n");
+
+        for  (int[] row : mat2) {
+            for (int i : row)
+                System.out.print(i + "\t");
+            System.out.println("\t\t" + getSum(row));
+        }
+        for (int lcv = 0; lcv < mat2[0].length; lcv++) {
+            System.out.print(getSum(mat2[lcv])+"\t");
+        }
+        System.out.println("\n\n\n");
+
+
+
+        if (isMagicSquare(mat2)) System.out.println("Matrix 2 is a Magic Square!");
+        else System.out.println("Matrix 2 is not  a Magic Square..");
+
     }
 
-    public boolean isMagicSquare(int[][] mat) {
+    public static boolean isMagicSquare(int[][] mat) {
         int sum = getSum(mat[0]);
         //Check Rows
-        for  (int i = 1; i < mat.length; i++) {
-            if (getSum(mat[i])!=sum)
+        for (int i = 1; i < mat.length; i++) {
+            if (getSum(mat[i]) != sum)
                 return false;
         }
         //Check Cols
-        for  (int i = 1; i < mat[0].length; i++) {
-            if (getSum(new int[] {mat[0][i]})!=sum)
+        for (int i = 1; i < mat[0].length; i++) {
+            if (getSum(mat[0]) != sum)
                 return false;
         }
+        // Check Diags
+        int tempsum = 0;
+        for (int r=1; r < mat.length; r++) {
+            for (int c=1; c < mat[0].length; c++) {
+                if (r==c) tempsum += mat[r][c];
+            }
+        }
+        if (tempsum != sum) return false;
 
+        tempsum = 0;
+        for  (int r = 1; r < mat.length; r++) {
+            for (int c = 1; c < mat[0].length; c++) {
+                if ((r+c)==2) tempsum += mat[r][c];
+            }
+        }
+        if (tempsum != sum) return false;
+        return true;
     }
 
-    public int getSum(int[] arr) {return Arrays.stream(arr).sum();}
+
+    public static int getSum(int[] arr) {
+        int sum = 0;
+        for (int i : arr)
+            sum += i;
+        return sum;
+    }
 }
-*/
