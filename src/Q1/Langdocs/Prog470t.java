a4080b39c82558c7df989f5f7f11d5f7db229b68
white.o3 <white.o3@stu.janesville.k12.wi.us>
2025-10-10 10:31:10 -0500

    470t first

--- src/Q1/Langdocs/Prog470t.java
+++ src/Q1/Langdocs/Prog470t.java
@@ -9,6 +9,7 @@ public class Prog470t {
         int[][] mat = new int[4][4];
         int[][] mat2 = new int[4][4];
 
+        // populate matrices
         while (file1.hasNextLine()) {
             for (int i = 0; i < 4; i++)
                 for (int j = 0; j < 4; j++)
@@ -20,68 +21,27 @@ public class Prog470t {
                     mat2[i][j] = file2.nextInt();
         }
 
-        for (int[] row : mat) {
-            for (int i : row)
-                System.out.print(i + "\t");
-            System.out.println("\t\t" + getSum(row));
-        }
-        if (isMagicSquare(mat)) System.out.println("Matrix 1 is a Magic Square!");
-        else System.out.println("Matrix 1 is not a Magic Square..");
-        System.out.println("\n\n\n");
-
-        for  (int[] row : mat2) {
-            for (int i : row)
-                System.out.print(i + "\t");
-            System.out.println("\t\t" + getSum(row));
-        }
-        for (int lcv = 0; lcv < mat2[0].length; lcv++) {
-            System.out.print(getSum(mat2[lcv])+"\t");
-        }
-        System.out.println("\n\n\n");
 
 
 
-        if (isMagicSquare(mat2)) System.out.println("Matrix 2 is a Magic Square!");
-        else System.out.println("Matrix 2 is not  a Magic Square..");
+
 
     }
+    boolean isMagicSqaure(int[][] mat) {
+        boolean myOut = true;
+        int[] rowSums = new int[4];
+        int[] colSums = new int[4];
 
-    public static boolean isMagicSquare(int[][] mat) {
-        int sum = getSum(mat[0]);
-        //Check Rows
-        for (int i = 1; i < mat.length; i++) {
-            if (getSum(mat[i]) != sum)
-                return false;
-        }
-        //Check Cols
-        for (int i = 1; i < mat[0].length; i++) {
-            if (getSum(mat[0]) != sum)
-                return false;
-        }
-        // Check Diags
-        int tempsum = 0;
-        for (int r=1; r < mat.length; r++) {
-            for (int c=1; c < mat[0].length; c++) {
-                if (r==c) tempsum += mat[r][c];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                //TODO
             }
+            //TODO
         }
-        if (tempsum != sum) return false;
 
-        tempsum = 0;
-        for  (int r = 1; r < mat.length; r++) {
-            for (int c = 1; c < mat[0].length; c++) {
-                if ((r+c)==2) tempsum += mat[r][c];
-            }
-        }
-        if (tempsum != sum) return false;
-        return true;
-    }
-    //commit message
 
-    public static int getSum(int[] arr) {
-        int sum = 0;
-        for (int i : arr)
-            sum += i;
-        return sum;
+
+
+        return myOut;
     }
 }
