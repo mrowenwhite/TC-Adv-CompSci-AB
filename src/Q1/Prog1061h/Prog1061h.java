dae47437e61fb015b16b119f3acb13d3ff3dca5a
white.o3 <white.o3@stu.janesville.k12.wi.us>
2025-10-16 09:45:36 -0500

    working on 1061h

--- src/Q1/Prog1061h/Prog1061h.java
+++ src/Q1/Prog1061h/Prog1061h.java
@@ -6,16 +6,13 @@ import java.util.*;
 
 class Prog1061h {
     public static void main(String[] args) throws IOException {
-        p1061Helper<String> list = new p1061Helper();
+        p1061Helper list = new p1061Helper();
         Scanner file = new Scanner(new File("Langdats/prg1061h.txt"));
 
         int cnt = 0;
         while (file.hasNextLine()) {
-            list.add(file.nextLine());
+            list.addAlphabetically(file.nextLine());
         }
-        System.out.println(list);
-
-
-
+        list.print();
     }
 }
--- src/Q1/Prog1061h/p1061Helper.java
+++ src/Q1/Prog1061h/p1061Helper.java
@@ -11,11 +11,40 @@ public class p1061Helper {
             this.data = data;
         }
 
+        public String getData() {
+            return data;
+        }
+    }
 
+    public p1061Helper() {
+        head = null;
     }
 
     public void addAlphabetically(String data) {
-        
+        if (head == null) {
+            head = new Node(data);
+        }
+        else {
+            Node temp = head;
+            while (temp.next != null) {
+                if (temp.next.getData().compareTo(data) <= 0) {
+                    Node newNode = new Node(data);
+                    newNode.next = temp.next;
+                    temp.next = newNode;
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
     }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.getData() + ", ");
+            temp = temp.next;
+        }
+    }
+
 
 }
