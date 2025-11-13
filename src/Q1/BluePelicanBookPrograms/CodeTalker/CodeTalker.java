4fbf2988b04cb86b6787a0e905303ecceb6d287b
white.o3 <white.o3@stu.janesville.k12.wi.us>
2025-11-13 10:12:21 -0600

    code talker and student classification done

--- src/Q1/BluePelicanBookPrograms/CodeTalker/CodeTalker.java
+++ src/Q1/BluePelicanBookPrograms/CodeTalker/CodeTalker.java
@@ -4,29 +4,15 @@ import java.io.*;
 
 public class CodeTalker {
     public static void main(String[] args) throws IOException {
-        Map<String, String> map = new HashMap<String, String>();
         Scanner file = new Scanner(new File("src/Q1/BluePelicanBookPrograms/CodeTalker/CodeTalkerData.dat"));
-        int cnt = Integer.parseInt(file.nextLine());
-        for (int i = 0; i <= cnt; i++) {
-            String line =  file.nextLine();
-            System.out.println(line);
-            //map.put(line.substring(0, line.indexOf('=')), line.substring(line.indexOf('=') + 1));
-            String[] arr = line.split("=");
-            System.out.println(arr.length);
-             map.put(arr[0], arr[1]);
+        Map<String, String> map = new HashMap<>();
+        int count = Integer.parseInt(file.nextLine());
+        for (int i = 0; i < count; i++) {
+            String[] line = file.nextLine().split("=");
+            map.put(line[0], line[1]);
         }
-
-        String[] words = file.nextLine().split(" ");
-        for (int i = 0; i < words.length; i++) {
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                if (words[i].equals(entry.getKey())) {
-                    System.out.print(entry.getValue());
-                }
-            }
-            System.out.println();
+        for (String s : file.nextLine().split(" ")) {
+            System.out.println(map.getOrDefault(s, s));
         }
-
     }
-
-
 }
