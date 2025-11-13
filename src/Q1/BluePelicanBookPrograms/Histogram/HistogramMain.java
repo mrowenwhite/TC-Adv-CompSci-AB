731e363ad10dc6204c8d6b3064accbf1807c05aa
white.o3 <white.o3@stu.janesville.k12.wi.us>
2025-11-13 10:31:19 -0600

    todo: hist, 850b, map1: coding bats

---
+++ src/Q1/BluePelicanBookPrograms/Histogram/HistogramMain.java
@@ -0,0 +1,12 @@
+package Q1.BluePelicanBookPrograms.Histogram;
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class HistogramMain {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("src/Q1/BluePelicanBookPrograms/Histogram/HistogramData.dat"));
+        HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+        System.out.println(map);
+    }
+}
