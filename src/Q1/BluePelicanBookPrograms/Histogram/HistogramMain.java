ac61cc992b9482fb5e65a3f68db67e0daa312617
O-white1 <white.o3@stu.janesville.k12.wi.us>
2025-11-13 21:12:32 -0600

    student class map prog done

--- src/Q1/BluePelicanBookPrograms/Histogram/HistogramMain.java
+++ src/Q1/BluePelicanBookPrograms/Histogram/HistogramMain.java
@@ -6,7 +6,14 @@ import java.util.stream.Collectors;
 public class HistogramMain {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("src/Q1/BluePelicanBookPrograms/Histogram/HistogramData.dat"));
-        HashMap<String, ArrayList<Integer>> map = new HashMap<>();
-        System.out.println(map);
+        Map<String, Integer> map = new HashMap<>();
+        while (sc.hasNextLine()) {
+            String[] split = sc.nextLine().split(",");
+            for (String s : split)
+                map.put(s, map.getOrDefault(s, 0) + 1);
+
+            for (String key : map.keySet())
+                System.out.println(key + " : " + map.get(key));
+        }
     }
 }
---
+++ src/Q1/Langdocs/Prog850b.java
@@ -0,0 +1,15 @@
+package Q1.Langdocs;
+import java.util.*;
+import java.io.*;
+
+public class Prog850b {
+    public static void main(String[] args) throws IOException {
+        Scanner file = new Scanner(new File("Langdats/prog850b.txt"));
+        Map<String, Integer> map = new HashMap<>();
+
+        while (file.hasNextLine()) {
+            //?
+        }
+
+    }
+}
