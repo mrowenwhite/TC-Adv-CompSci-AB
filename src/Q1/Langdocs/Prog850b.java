436cfba45a2a8475266736799eca6c06f8d16379
white.o3 <white.o3@stu.janesville.k12.wi.us>
2025-11-14 10:31:36 -0600

    850b cooking me

--- src/Q1/Langdocs/Prog850b.java
+++ src/Q1/Langdocs/Prog850b.java
@@ -4,12 +4,17 @@ import java.io.*;
 
 public class Prog850b {
     public static void main(String[] args) throws IOException {
-        Scanner file = new Scanner(new File("Langdats/prog850b.txt"));
+        Scanner file = new Scanner(new File("Langdats/Prog850b.csv"));
         Map<String, Integer> map = new HashMap<>();
 
         while (file.hasNextLine()) {
-            //?
+            String[] line =  file.nextLine().split(",");
+            if (!line[1].equals("total"))map.put(line[0], map.getOrDefault(line[0], Integer.parseInt(line[5])));
         }
+        for  (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
 
     }
 }
