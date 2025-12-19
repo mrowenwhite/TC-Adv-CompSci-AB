eeefbc5ed239cd478fc58d113ea9b5afecf9df32
white.o3 <white.o3@stu.janesville.k12.wi.us>
2025-12-19 10:30:48 -0600

    Big Tree Almost Done

--- src/Sem1/BigBinaryTree/BinaryTree.java
+++ src/Sem1/BigBinaryTree/BinaryTree.java
@@ -1,5 +1,8 @@
 package Sem1.BigBinaryTree;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class BinaryTree {
     private btNode root;
     static class btNode {
@@ -132,4 +135,92 @@ public class BinaryTree {
 
         }
     }
+
+    public boolean contains(int data) {
+        btNode temp = root;
+        while (temp!=null) {
+            if (data==temp.data) {return true;}
+            if (data<temp.data) {temp=temp.left;}
+            else {temp=temp.right;}
+        }
+        return false;
+    }
+
+
+    private int DFS_depth(btNode n) {
+        if (n==null) {return 0;}
+        return 1 + Math.max(DFS_depth(n.left), DFS_depth(n.right));
+    }
+    public int DFS_depth() {return DFS_depth(root);}
+
+
+    public int BFS_width() {
+        Queue<btNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int maxWid = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                btNode node = queue.poll();
+                if (node.left != null) {queue.add(node.left);}
+                if (node.right!= null) {queue.add(node.right);}
+            }
+            size  = queue.size();
+            maxWid = Math.max(maxWid, size);
+        }
+        return maxWid;
+    }
+
+    public int size() {
+        Queue<btNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int size = 0;
+        while (!queue.isEmpty()) {
+            int Qsize = queue.size();
+            for (int i = 0; i <Qsize; i++) {
+                btNode node = queue.poll();
+                if (node.left != null) {queue.add(node.left);}
+                if (node.right != null) {queue.add(node.right);}
+                size++;
+            }
+            size  = queue.size();
+        }
+        return size;
+    }
+
+    private int NodeSum() {
+        Queue<btNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                btNode node = queue.poll();
+                if (node.left != null) {queue.add(node.left);}
+                if (node.right!= null) {queue.add(node.right);}
+                sum += node.data;
+            }
+            size  = queue.size();
+        }
+        return sum;
+    }
+    public int AverageValue() {
+        return NodeSum()/this.size();
+    }
+
+    public void KillClosestToAvg() {
+        int avg = AverageValue();
+        Queue<btNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                btNode node = queue.poll();
+                if (node.left != null) {queue.add(node.left);}
+                if (node.right != null) {queue.add(node.right);}
+            }
+            size  = queue.size();
+        }
+    }
+
 }
--- src/Sem1/BigBinaryTree/bigBTmain.java
+++ src/Sem1/BigBinaryTree/bigBTmain.java
@@ -1,16 +1,40 @@
 package Sem1.BigBinaryTree;
 
+import java.util.Random;
+
 public class bigBTmain {
     public static void main(String[] args) {
-        int[] list = {4,2,8,5,1,6,3,9,7};
-        BinaryTree bt = new BinaryTree();
-        bt.add(list);
-        bt.printIntOrder();System.out.println();
-        bt.delete(7);
-        bt.printIntOrder();System.out.println();
-        bt.delete(6);
-        bt.printIntOrder();System.out.println();
-        bt.delete(8);
-        bt.printIntOrder();System.out.println();
+        BinaryTree binaryTree = new BinaryTree();
+        Random rand = new Random();
+        //1
+        for (int i =0; i <= 50; i ++) {
+            int n = rand.nextInt(100);
+            binaryTree.add(n);
+            //2
+            System.out.print(n +" ");
+        }
+        System.out.println();
+        //3
+        System.out.print("NLR:\t");binaryTree.printPREFIX(); System.out.println();
+        System.out.print("LNR:\t");binaryTree.printINFIX();  System.out.println();
+        System.out.print("LRN:\t");binaryTree.printPOSTFIX();System.out.println();
+        System.out.println("\n\n");
+        //4
+        System.out.println(binaryTree.contains(32));
+        System.out.println("\n\n");
+        //5
+        System.out.println(binaryTree.DFS_depth());
+        System.out.println("\n\n");
+        //6
+        System.out.println("Width:\t" + binaryTree.BFS_width());
+        System.out.println("\n\n");
+        //7
+        binaryTree.add(new int[]{51, 102});
+        binaryTree.printPREFIX();
+        System.out.println("\n\n");
+        //8
+        int size = binaryTree.size();
+
+
     }
 }
