a3dfabb8a9558541c86467d845a8d82789d43c29
white.o3 <white.o3@stu.janesville.k12.wi.us>
2026-01-08 10:31:10 -0600

    working on Giga Farm and 1060z

--- src/Sem1/GigaFarm_1999W/Animals/Horse.java
+++ src/Sem1/GigaFarm_1999W/Animals/Horse.java
@@ -3,18 +3,39 @@ package Sem1.GigaFarm_1999W.Animals;
 import java.util.*;
 
 public class Horse extends Animal{
-
+    String name;
+    double rideCost;
     public Horse() {
         super();
+        name = makeName();
+        rideCost = (Math.random()*10.5-7.01)+7.0;
 
     }
+
+
+
+
+
+
+
     public String makeName() {
         char[] letters = {'a','b','c','d','e','f','g', 'h','j','k','l','m','n','o','p', 'q','r','s','t','u','v','w','x', 'y','z'};
         char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
-        String name = "####";
-        for (int i = 0; i < name.length(); i++) {
+        char[] name = new char[4];
+        for (int i = 0; i < name.length; i++) {
             char letter = letters[(int)(Math.random()*25-1)];
+            if (i!=2) {
+                while(List.of(vowels).contains(letter)) {
+                    letter = letters[(int)(Math.random()*25-1)];
+                }
+            }
+            else {
+                while (!List.of(letters).contains(letter)) {
+                    letter = letters[(int)(Math.random()*25-1)];
+                }
+            }
+            name[i] = letter;
         }
-
+        return String.valueOf(name);
     }
 }
--- src/Sem1/GigaFarm_1999W/Animals/Turkey.java
+++ src/Sem1/GigaFarm_1999W/Animals/Turkey.java
@@ -2,13 +2,16 @@ package Sem1.GigaFarm_1999W.Animals;
 
 public class Turkey extends Animal {
     private final double lbVal;
-    public Turkey() {
+    private final int code;
+    public Turkey(int code) {
         super();
         this.weight = (int)((Math.random()*35-26)+25);
         this.lbVal = (Math.random()*0.99-76)+0.75;
+        this.code = code;
 
         this.foods.put("corn", (int)(Math.random()*3-2)+1); //1-3 corns
         this.foods.put("oats", (int)(Math.random()*4-3)+2); // 2-4 oats
     }
     public double getVal() {return lbVal;}
+    public int getCode() {return code;}
 }
