0adf2645ec2ce5bc89374d134e0667bfabc8a2ec
white.o3 <white.o3@stu.janesville.k12.wi.us>
2026-01-21 11:12:15 -0600

    96% in AP2 for WhiteWater, Adios!

--- src/Sem1/GigaFarm_1999W/Farm.java
+++ src/Sem1/GigaFarm_1999W/Farm.java
@@ -18,9 +18,17 @@ public class Farm {
         foodStock.put("oats",     (int)(Math.random()*2500-1699)+1700);
 
 
-        for (int lcv = 0; lcv < (int)((Math.random()*15-11)+12);  lcv++) {
-            cows.put(makeCode(), new Cow());
-        }
+        for (int lcv = 0; lcv < (int)((Math.random()*15-11)+12);  lcv++) {cows.put(makeCode(4), new Cow());}
+        for (int lcv = 0; lcv < 20; lcv++) {pigs[lcv] = new Pig();}
+        for (int lcv = 0; lcv < ((int)(Math.random()*20-11)+10);lcv++) {turkeys.add(new Turkey(3));}
+
+            /*
+            Each farm is to have a Map of between 12 and
+15 cows. Each farm is to have a Set of between 10 and 20 Turkeys. Each farm is to have a circle linked
+list of between 8 and 10 horses. Each farm will have 20 pens (an array) to hold between 12 and 20 pigs.
+Each farm sets its own milk price and the price should be between 20 and 24 cents per pound.
+             */
+
 
         //populate rider matrix
         for (int i = 0; i < riders.length; i++)
@@ -28,8 +36,8 @@ public class Farm {
                 riders[i][j] = (i==5) ? (int)(Math.random()*10-6)+5 : (i==6) ? (int)(Math.random()*7-4)+3 : (int)(Math.random()*5-2)+1;
     }
 
-    public String makeCode() {
-        char[] chars = {'0','0','0','0'};
+    public String makeCode(int n) {
+        char[] chars = new char[n];
         for  (int i = 0; i < chars.length; i++)
             chars[i] = (char)(Math.random()*'9');
         return new String(chars);
