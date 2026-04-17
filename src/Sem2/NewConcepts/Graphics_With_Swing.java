72be5ca9abb98726d985711dd1a0a6b68768e997
white.o3 <white.o3@stu.janesville.k12.wi.us>
2026-04-17 10:30:52 -0500

    working on jframe

--- src/Sem2/NewConcepts/Graphics_With_Swing.java
+++ src/Sem2/NewConcepts/Graphics_With_Swing.java
@@ -2,30 +2,12 @@ package Sem2.NewConcepts;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
-import javax.rmi.ssl.SslRMIClientSocketFactory;
 import javax.swing.*;
 import java.io.*;
 
 public class Graphics_With_Swing {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setBounds(800, 450, 800, 450); // centering stuff
-        frame.setVisible(true);
+        TemplateFrame1 frame1 = new TemplateFrame1(854, 480);
 
-
-        JButton exit = new JButton("Quit");
-        frame.add(exit);
-        exit.setBounds(frame.getX()/2, frame.getY()/2, frame.getWidth()/5, frame.getHeight()/5);
-        exit.setHorizontalAlignment(JButton.CENTER);
-        exit.setVerticalAlignment(JButton.CENTER);
-
-        exit.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) {System.exit(0);}});
-
-
-        JLabel label = new JLabel("test");
-        label.setAlignmentX(Component.CENTER_ALIGNMENT);
-        label.setAlignmentY(Component.CENTER_ALIGNMENT);
-        label.setBounds(frame.getX()/2, frame.getY()/2, frame.getX()/20, frame.getY()/20);
-        frame.add(label);
     }
 }
