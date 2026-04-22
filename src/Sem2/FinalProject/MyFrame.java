a00110fbaba4a53613ede900edcf8abc6681dfe3
white.o3 <white.o3@stu.janesville.k12.wi.us>
2026-04-22 10:32:06 -0500

    making homemade jtable for transaction list

--- src/Sem2/FinalProject/MyFrame.java
+++ src/Sem2/FinalProject/MyFrame.java
@@ -9,7 +9,11 @@ import javax.swing.table.TableModel;
 
 public class MyFrame extends JFrame {
     JPanel header;
+    JTable table;
     ArrayList<Transaction> list;
+    JTextField amounts;
+    JComboBox<String> categories;
+    JComboBox<String> descriptions;
 
     MyFrame(int len, int wid) {
 
@@ -17,27 +21,42 @@ public class MyFrame extends JFrame {
         this.setVisible(true);
         this.setLayout(new FlowLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        list         = new ArrayList<>();
+        header       = new JPanel();
+        table        = new JTable(new DefaultTableModel(3, 0));
+        amounts      = MakeAmountBox();
+        categories   = MakeCategoryBox();
+        descriptions = MakeDescriptionBox();
 
-        header = new JPanel();
-        JComboBox<String> categories = MakeCategoryBox();
-        JTextField amounts = MakeAmountBox();
-        JComboBox<String> descriptions = MakeDescriptionBox();
         header.add(new JLabel("Category: "));header.add(categories);
         header.add(new JLabel("Amount: "));header.add(amounts);
         header.add(new JLabel("Description: "));header.add(descriptions);
+        table.setModel(new DefaultTableModel(3, 3));
 
-        JTable table = new JTable();
-        table.setModel(new DefaultTableModel(3, 10));
+        JButton button = getJButton();
+        header.add(button);
+        this.add(header, BorderLayout.NORTH);
         this.add(table, BorderLayout.CENTER);
+    }
+
+    private JButton getJButton() {
         JButton button = new JButton("Add Transaction");
         button.addActionListener(e ->{
             Transaction t = new Transaction((String)categories.getSelectedItem(), Integer.parseInt(amounts.getText()), (String)descriptions.getSelectedItem());
             list.add(t);
             System.out.println(t);
+            for (int lcv = 0; lcv < list.size(); lcv++) {
+                Transaction t1 = list.get(lcv);
+                table.add
+                // ADD ROWS WITH THE MODEL< TALK TO LANDON!!!!!!!!!
+
+                table.setValueAt(t1.category(),    lcv, 0);
+                table.setValueAt(t1.amount(),      lcv, 1);
+                table.setValueAt(t1.description(), lcv, 2);
+            }
 
         });
-        header.add(button);
-        this.add(header, BorderLayout.NORTH);
+        return button;
     }
 
     private JComboBox<String> MakeCategoryBox() {
@@ -62,5 +81,9 @@ public class MyFrame extends JFrame {
         comboBox.addItem("Option 3");
         comboBox.addItem("Option 4");
         return comboBox;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        list.add(transaction);
     }
 }
