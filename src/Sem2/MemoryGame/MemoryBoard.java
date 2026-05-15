d3cb3b80cebda66df7f85c5da1210a9085ca7f01
white.o3 <white.o3@stu.janesville.k12.wi.us>
2026-05-15 10:30:08 -0500

    memoryboard, hidetiles

--- src/Sem2/MemoryGame/MemoryBoard.java
+++ src/Sem2/MemoryGame/MemoryBoard.java
@@ -1,19 +1,60 @@
 package Sem2.MemoryGame;
 
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.stream.IntStream;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MemoryBoard {
+public class MemoryBoard extends JFrame {
+    static Map<MyButton, MyButton> pairs = new HashMap<>();
+    static MyButton lastClicked = null;
 
-     public MemoryBoard(int n) {
-          MyButton[][] board = getBoard();
+
+    public static class MyButton extends JButton {
+        static Timer timer = new Timer(750, e->HideTiles());
+        public boolean isFinished;
+
+        public MyButton(Color color) {
+            timer.setRepeats(false);
+            isFinished = false;
+            this.setSize(20, 20);
+            this.setBackground(Color.DARK_GRAY);
+
+            this.addActionListener(e -> {
+                secondClicked=this;
+                this.setBackground(color);
+                if ((pairs.get(firstClicked) != null)&&(pairs.get(firstClicked)==this)) {
+                    this.setBackground(color);
+                    firstClicked.setBackground(color);
+                    isFinished = true;
+                    secondClicked.isFinished = true;
+
+                }
+                else {
+                    timer.start();
+                    while  (timer.isRunning())continue;
+                    timer.stop();
+                }
+            });
+        }
+
+        public static void HideTiles() {} //TODO
+    }
+    public MemoryBoard() {
+        this.setTitle("Memory Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(842, 480);
+        this.setVisible(true);
+        this.setLayout(new GridLayout(4, 4, 20, 20));
 
 
-     }
+        MyButton[][] board = getBoard();
+        for (MyButton[]row: board)for(MyButton temp:row)this.add(temp);
+        this.revalidate();
+    }
 
      public MyButton[][] getBoard() {
          ArrayList<MyButton> list   = new ArrayList<>();
@@ -23,17 +64,20 @@ public class MemoryBoard {
          for (Color color : COLORS) {
              list.add(new MyButton(color));
              list.add(new MyButton(color));
+             pairs.put(list.getLast(), list.get(list.size()-2));
+             pairs.put(list.get(list.size()-2), list.getLast());
          }
          Collections.shuffle(list);
 
          int cnt = 0;
-         for (int lcv = 0; lcv < 4; lcv++) {
-             for (int lcv2 = 0; lcv2 < 4; lcv2++) {
-                 board[lcv][lcv2] = list.get(cnt);
-                 cnt++;
-             }
-         }
+         for (int lcv = 0; lcv < 4; lcv++)
+             for (int lcv2 = 0; lcv2 < 4; lcv2++)
+                 board[lcv][lcv2] = list.get(cnt++);
          return board;
      }
+
+    public static void main(String[] args) {
+        new MemoryBoard();
+    }
 
 }
