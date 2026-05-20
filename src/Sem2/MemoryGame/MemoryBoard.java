fe66b1bea782e29531a91b6ef1cd48aed94dff3b
white.o3 <white.o3@stu.janesville.k12.wi.us>
2026-05-20 10:48:36 -0500

    Fineto

--- src/Sem2/MemoryGame/MemoryBoard.java
+++ src/Sem2/MemoryGame/MemoryBoard.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MemoryBoard extends JFrame {
     static MemoryBoard memoryBoard;
@@ -13,14 +14,18 @@ public class MemoryBoard extends JFrame {
     static MyButton[][] Board;
     static MyButton firstClicked;
     static MyButton secondClicked;
+    static boolean isPlaying;
     static Timer timer = new Timer(750, e-> {
         for (MyButton[] row: Board )for(MyButton temp:row){
-            temp.setBackground((temp.isFinished)?temp.color:Color.DARK_GRAY);
-            temp.setEnabled(true);
+            if (isPlaying) {
+                temp.setBackground((temp.isFinished)?temp.color:Color.DARK_GRAY);
+                temp.setEnabled(true);
+            }
         }
     });
+
+
     static int pairCount;
-    static boolean isPlaying;
 
     public static class MyButton extends JButton {
 
@@ -34,7 +39,6 @@ public class MemoryBoard extends JFrame {
             this.setSize(20, 20);
             this.setBackground(Color.DARK_GRAY);
             firstClicked = null;secondClicked = null;
-
             this.addActionListener(e -> {
                 this.setOpaque(true);this.revalidate();this.repaint();
                 if (isPlaying) {
@@ -47,7 +51,8 @@ public class MemoryBoard extends JFrame {
                             firstClicked.isFinished=true;firstClicked.setBackground(color);
                             secondClicked.isFinished=true;secondClicked.setBackground(color);
                             pairCount--;
-                            if (pairCount <= 0) {JOptionPane.showMessageDialog(memoryBoard, "All Pairs Matched");memoryBoard.dispose();}
+
+                            if (pairCount <= 0) {JOptionPane.showMessageDialog(memoryBoard, "All Pairs Matched");isPlaying=false;memoryBoard.dispose();}
                             SetTilesEnabled(true);
                         }
                         else {timer.start();}
@@ -71,11 +76,13 @@ public class MemoryBoard extends JFrame {
         Board = getBoard();
         for (MyButton[]row: Board)for(MyButton temp:row)this.add(temp);
         JButton play = new JButton("Play");
-        play.addActionListener(e -> {isPlaying=true;});
+        GameTimer GT = new GameTimer(60);
+        this.add(GT, CENTER_ALIGNMENT);
+        play.addActionListener(e -> {isPlaying=true;GT.start();});
         this.add(play, CENTER_ALIGNMENT);
         this.revalidate();
     }
-     public static MyButton[][] getBoard() {
+    public static MyButton[][] getBoard() {
          ArrayList<MyButton> list   = new ArrayList<>();
          MyButton[][]        board  = new MyButton[4][4];
          final Color[]       COLORS = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.PINK, Color.MAGENTA, Color.WHITE};
@@ -92,4 +99,27 @@ public class MemoryBoard extends JFrame {
      }
     public static void SetTilesEnabled(boolean b){for (MyButton[] row: Board )for(MyButton temp:row) temp.setEnabled(b);}
     public static void main(String[] args) {new MemoryBoard();}
+
+
+    public static class GameTimer extends JLabel {
+        private int TimeLeft;
+        Timer timer = new Timer(1000, e-> {
+            TimeLeft--;
+            this.setText("TimeLeft: "+TimeLeft/60 + ":" + TimeLeft%60);
+            if (TimeLeft <= 0) {
+                isPlaying = false;
+                JOptionPane.showMessageDialog(memoryBoard, "You Lose!");
+                memoryBoard.dispose();
+                return;
+            }
+        });
+        GameTimer(int time) {
+            this.setText("TimeLeft: "+TimeLeft/60 + ":" + TimeLeft%60);
+            TimeLeft = time;
+        }
+        public void start() {
+            timer.start();
+        }
+
+    }
 }
