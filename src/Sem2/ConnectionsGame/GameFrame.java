939cceab9982197b19762e33eb81df8774a46bd0
white.o3 <white.o3@stu.janesville.k12.wi.us>
2026-05-28 10:30:20 -0500

    out of bounds for line 51 of GameFrame

--- src/Sem2/ConnectionsGame/GameFrame.java
+++ src/Sem2/ConnectionsGame/GameFrame.java
@@ -1,6 +1,7 @@
 package Sem2.ConnectionsGame;
 
 import javax.swing.*;
+import javax.xml.catalog.Catalog;
 import java.awt.*;
 import java.io.*;
 import java.util.*;
@@ -13,6 +14,11 @@ public class GameFrame extends JFrame {
 
     public GameFrame() throws FileNotFoundException {
         GameBuilder();
+        this.setTitle("Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.setVisible(true);
+        this.revalidate();
     }
 
 
@@ -23,9 +29,9 @@ public class GameFrame extends JFrame {
         Scanner file = new Scanner(new File("C:\\Users\\white.o3\\IdeaProjects\\TC CompSci\\src\\Sem2\\ConnectionsGame\\ConnectionsFile.txt"));
         Map<String, Stack<String>> map = new HashMap<>();
         Random random = new Random();
-        String[] categories = new String[4];
-        String[] items = new String[16];
-        int Ilcv = 0;
+
+
+        ArrayList<String> items = new ArrayList<>();
         while (file.hasNextLine()) {
             String line = file.nextLine();
             if (line.charAt(line.length()-1)==':') {
@@ -37,27 +43,24 @@ public class GameFrame extends JFrame {
                     line = file.nextLine();
                 }
             }
-        } //Populate Categories From File
-        for  (int i = 0; i < 4; i++) {
-            int n = random.nextInt(categories.length);
-            boolean condition = true;
-            try {if (List.of(categories).contains(map.keySet().stream().toList().get(n)))condition = true;} catch (Exception e) {condition = false;}
-            while (condition) n = random.nextInt(categories.length); // when get(n) is null, contains throws NPE
-            categories[i] = map.keySet().stream().toList().get(n);
+        } //Populate Categories From File // populates map
+        ArrayList<String> TempCats = new ArrayList<>(map.keySet());
+        Collections.shuffle(TempCats);
+        String[] categories = new String[4];
+        for (int i = 0; i < 4; i++) {
+            categories[i] = TempCats.get(i); // INDEX OUT OF BOUNDS
         }
          // populate categories for gameBoard, no repeats
         for (int row = 0; row < 4; row++) {
             for (int col = 0; col < 4; col++) {
-                items[Ilcv] = map.get(categories[row]).pop();
-                Ilcv++;
+                items.add(map.get(categories[row]).pop());
             }
         } // create Items for gameBoard based on categories
         Collections.shuffle(Arrays.asList(items));
-        Ilcv = 0;
+        int Ilcv = -1;
         for (int r = 0; r < 4; r++) {
             for (int c = 0; c < 4; c++) {
-                GameBoard[r][c] = new JToggleButton(items[Ilcv]);
-                Ilcv++;
+                GameBoard[r][c] = new JToggleButton(items.get(Ilcv++));
             }
         } // populate GameBoard based on items
         this.setLayout(new GridLayout(4,4));
